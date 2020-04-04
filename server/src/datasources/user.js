f5886d9621fd6cf54f20050a78d0fd861c0a85f7
mecha2k <mecha2k@naver.com>
2020-04-04 12:20:49 +0900

    1st commit.

--- server/src/datasources/user.js
+++ server/src/datasources/user.js
@@ -1,5 +1,5 @@
-const { DataSource } = require('apollo-datasource');
-const isEmail = require('isemail');
+const { DataSource } = require("apollo-datasource");
+const isEmail = require("isemail");
 
 class UserAPI extends DataSource {
   constructor({ store }) {
@@ -7,24 +7,19 @@ class UserAPI extends DataSource {
     this.store = store;
   }
 
-  /**
-   * This is a function that gets called by ApolloServer when being setup.
-   * This function gets called with the datasource config including things
-   * like caches and context. We'll assign this.context to the request context
-   * here, so we can know about the user making requests
-   */
+  // This is a function that gets called by ApolloServer when being setup. This function
+  // gets called with the datasource config including things like caches and context. We'll
+  // assign this.context to the request context here, so we can know about the user making requests
+
   initialize(config) {
     this.context = config.context;
   }
 
-  /**
-   * User can be called with an argument that includes email, but it doesn't
-   * have to be. If the user is already on the context, it will use that user
-   * instead
-   */
+  // User can be called with an argument that includes email, but it doesn't have to be.
+  // If the user is already on the context, it will use that user instead
+
   async findOrCreateUser({ email: emailArg } = {}) {
-    const email =
-      this.context && this.context.user ? this.context.user.email : emailArg;
+    const email = this.context && this.context.user ? this.context.user.email : emailArg;
     if (!email || !isEmail.validate(email)) return null;
 
     const users = await this.store.users.findOrCreate({ where: { email } });
@@ -50,7 +45,7 @@ class UserAPI extends DataSource {
   async bookTrip({ launchId }) {
     const userId = this.context.user.id;
     const res = await this.store.trips.findOrCreate({
-      where: { userId, launchId },
+      where: { userId, launchId }
     });
     return res && res.length ? res[0].get() : false;
   }
@@ -63,18 +58,16 @@ class UserAPI extends DataSource {
   async getLaunchIdsByUser() {
     const userId = this.context.user.id;
     const found = await this.store.trips.findAll({
-      where: { userId },
+      where: { userId }
     });
-    return found && found.length
-      ? found.map(l => l.dataValues.launchId).filter(l => !!l)
-      : [];
+    return found && found.length ? found.map(l => l.dataValues.launchId).filter(l => !!l) : [];
   }
 
   async isBookedOnLaunch({ launchId }) {
     if (!this.context || !this.context.user) return false;
     const userId = this.context.user.id;
     const found = await this.store.trips.findAll({
-      where: { userId, launchId },
+      where: { userId, launchId }
     });
     return found && found.length > 0;
   }
--- server/src/index.js
+++ server/src/index.js
@@ -3,3 +3,7 @@ const typeDefs = require("./schema");
 
 // noinspection JSCheckFunctionSignatures
 const server = new ApolloServer({ typeDefs });
+
+server.listen({ port: 3500 }).then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
