537084f4b5b5a8ac8e6820f18cb07263f44afbae
mecha2k <mecha2k@naver.com>
2020-04-05 07:05:45 +0900

    1st commit.

--- server/src/index.js
+++ server/src/index.js
@@ -13,21 +13,22 @@ const store = createStore();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+
   dataSources: () => ({
     launchAPI: new LaunchAPI(),
     userAPI: new UserAPI({ store })
-  }),
-  context: async ({ req }) => {
-    // simple auth check on every request
-    const auth = (req.headers && req.headers.authorization) || "";
-    const email = Buffer.from(auth, "base64").toString("ascii");
-    if (!isEmail.validate(email)) return { user: null };
-    // find a user by their email
-    const users = await store.users.findOrCreate({ where: { email } });
-    const user = (users && users[0]) || null;
-
-    return { user: { ...user.dataValues } };
-  }
+  })
+  // context: async ({ req }) => {
+  //   // simple auth check on every request
+  //   const auth = (req.headers && req.headers.authorization) || "";
+  //   const email = Buffer.from(auth, "base64").toString("ascii");
+  //   if (!isEmail.validate(email)) return { user: null };
+  //   // find a user by their email
+  //   const users = await store.users.findOrCreate({ where: { email } });
+  //   const user = (users && users[0]) || null;
+  //
+  //   return { user: { ...user.dataValues } };
+  // }
 });
 
 server.listen({ port: 3500 }).then(({ url }) => {
