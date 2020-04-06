4d6456b947f23761c0787cd6a2bbf3cfc7a8fb2d
mecha2k <mecha2k@naver.com>
2020-04-06 18:01:29 +0900

    1st commit

---
+++ client/src/index.tsx
@@ -0,0 +1,26 @@
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache, NormalizedCacheObject } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import gql from "graphql-tag";
+
+const cache = new InMemoryCache();
+const link = new HttpLink({ uri: "http://localhost:3500/" });
+
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({ cache, link });
+
+client
+  .query({
+    query: gql`
+      query GetLaunch {
+        launch(id: 56) {
+          id
+          mission {
+            name
+          }
+        }
+      }
+    `,
+  })
+  .then(function (result) {
+    console.log(result);
+  });
