befee92c4db30145f668bc0e0c3d33beeff24dfd
mecha2k <mecha2k@naver.com>
2020-04-06 18:15:27 +0900

    1st commit

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -18,7 +18,7 @@ const cache = new InMemoryCache();
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   cache,
   link: new HttpLink({
-    uri: "http://localhost:4000/graphql",
+    uri: "http://localhost:3500/graphql",
     headers: {
       authorization: localStorage.getItem("token"),
       "client-name": "Space Explorer [web]",
